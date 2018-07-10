8ba0f024b0db97dabc0a24fcdaf280403ea790cf
bmosqueda <bmosqueda@ucol.mx>
2018-07-10 12:51:50 -0500

    bootstrap y axios

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 //http://siabuc.ucol.mx:3001/api/fichas/busqueda?searchString=afsasdf
 const express = require('express');
 const path = require('path');
-
+ 
 const app = express();
 
 app.get('/prueba', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/prueba.html'));
+  res.sendFile(path.join(__dirname, '/src/'));
   console.log('En prueba');
 });
 
-app.use(express.static(path.join(__dirname,'/public')))
-app.use(express.static(path.join(__dirname,'/views')))
+app.use(express.static(path.join(__dirname,'/public')));
+app.use(express.static(path.join(__dirname,'/views')));
+app.use(express.static(path.join(__dirname,'/node_modules/axios/dist')));
 
 app.listen(3000);
